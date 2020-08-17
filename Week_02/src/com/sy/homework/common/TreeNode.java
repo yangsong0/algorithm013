a4ab03f46aa7804c83771a10b67dcef2b3d08c99
songyang <1075023046@qq.com>
2020-08-17 19:26:03 +0800

    TreeNode节点增加层序遍历方法

--- Week_02/src/com/sy/homework/common/TreeNode.java
+++ Week_02/src/com/sy/homework/common/TreeNode.java
@@ -1,6 +1,8 @@
 package com.sy.homework.common;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class TreeNode {
@@ -49,6 +51,26 @@ public class TreeNode {
             }
         }
         return root;
+    }
+
+    public static List<Integer> levelOrder(TreeNode root){
+        List<Integer> list=new ArrayList<>();
+        if(root==null){
+            return list;
+        }
+        Queue<TreeNode> queue=new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            TreeNode node=queue.poll();
+            list.add(node.val);
+            if(node.left!=null){
+                queue.offer(node.left);
+            }
+            if(node.right!=null){
+                queue.offer(node.right);
+            }
+        }
+        return list;
     }
 
     public static void main(String[] args) {
